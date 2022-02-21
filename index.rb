e1c9ee548bcd24a784d062da6b4d598190c6193e
Bazlur Rashid <bazlur.r@devandgo.com>
2022-02-21 11:39:09 +0000

    step 2

--- index.rb
+++ index.rb
@@ -1,13 +1,32 @@
 class Person
-  def set_hight(h)
-    @height = h
+
+  def set_age(age)
+    @age = age
   end
 
-  def show_height
-    return @height
+  def life_stage
+    create_life_stage
+  end
+
+  private
+  def create_life_stage
+    case @age
+    when 0..1
+      type = "INFANT"
+    when 2..12
+      type = "CHILD"
+    when 13..19
+      type = "YOUNG"
+    when 20..50
+      type = "ADULT"
+    else
+      type = "OLD"
+    end
+    type
   end
 end
 
 p = Person.new
-p.set_hight("180cm")
-puts p.show_height
+p.set_age(0)
+
+puts p.life_stage
