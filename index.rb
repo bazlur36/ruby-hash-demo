060b6a5fc7b1b499259f3a541a9fbb205825a17d
Bazlur Rashid <bazlur.r@devandgo.com>
2022-02-21 14:22:14 +0000

    step 1

--- index.rb
+++ index.rb
@@ -5,25 +5,9 @@ class Person
   end
 
   def life_stage
-    create_life_stage
+    @age
   end
 
-  private
-  def create_life_stage
-    case @age
-    when 0..1
-      type = "INFANT"
-    when 2..12
-      type = "CHILD"
-    when 13..19
-      type = "YOUNG"
-    when 20..50
-      type = "ADULT"
-    else
-      type = "OLD"
-    end
-    type
-  end
 end
 
 p = Person.new
