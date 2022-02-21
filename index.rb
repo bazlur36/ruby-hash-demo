4fd9105ec8932f0f480270d847419db44bc39ddb
Bazlur Rashid <bazlur.r@devandgo.com>
2022-02-21 12:51:48 +0000

    step 6

--- index.rb
+++ index.rb
@@ -9,20 +9,104 @@ class Prescription
   end
 
   private
+
   def create_prescription
     case @age
     when 0..1
-      shifts = {ageGroup: 'INFANT', :type => 'Syrup', :morning => '1 Spoon', :noon => '0.5 Spoon', :evening => '1 Spoon', :lateNight => '2 Spoons', }
+      prescription = {
+          ageGroup: 'INFANT',
+          medicines: [
+              {
+                  :type => 'Syrup',
+                  :name => 'Napa',
+                  :morning => '1 Spoon',
+                  :noon => '0.5 Spoon',
+                  :evening => '1 Spoon',
+                  :lateNight => '2 Spoons'
+              }
+          ]
+      }
     when 2..12
-      shifts = {ageGroup: 'CHILD', :type => 'Tablet', :morning => '1 tablet', :noon => '1 tablet', :evening => '1 tablet'}
+      prescription = {
+          ageGroup: 'CHILD',
+          medicines: [
+              {
+                  :type => 'Syrup',
+                  :name => 'Napa',
+                  :morning => '1',
+                  :evening => '1',
+              }
+          ]
+      }
     when 13..19
-      shifts = {ageGroup: 'YOUNG', :type => 'Tablet', :morning => '1 tablet', :evening => '1 tablet'}
+      prescription = {
+          ageGroup: 'CHILD',
+          medicines: [
+              {
+                  :type => 'Tablet',
+                  :name => 'Napa',
+                  :morning => '1',
+                  :evening => '1',
+                  :lateNight => '2'
+              },
+              {
+                  :type => 'Tablet',
+                  :name => 'Cevit',
+                  :morning => '1',
+                  :evening => '1',
+                  :lateNight => '2'
+              }
+          ]
+      }
     when 20..50
-      shifts = {ageGroup: 'ADULT', :type => 'Tablet', :morning => '1 tablet', :evening => '1 tablet' }
+      prescription = {
+          ageGroup: 'ADULT',
+          medicines: [
+              {
+                  :type => 'Tablet',
+                  :name => 'Napa',
+                  :morning => '1',
+                  :evening => '1',
+                  :lateNight => '2'
+              },
+              {
+                  :type => 'Tablet',
+                  :name => 'Calbo D',
+                  :morning => '1',
+                  :evening => '1',
+                  :lateNight => '2'
+              }
+          ]
+      }
     else
-      shifts = {ageGroup: 'OLD', :type => 'Syrup', :morning => '1 Spoon', :evening => '1 Speen' }
+      prescription = {
+          ageGroup: 'OLD',
+          medicines: [
+              {
+                  :type => 'Tablet',
+                  :name => 'Napa',
+                  :morning => '1',
+                  :evening => '1',
+                  :lateNight => '2'
+              },
+              {
+                  :type => 'Syrup',
+                  :name => 'Basok',
+                  :morning => '1 Spoon',
+                  :evening => '1 Spoon',
+                  :lateNight => '2 Spoon'
+              },
+              {
+                  :type => 'Injection',
+                  :name => 'Insuline',
+                  :morning => '1',
+                  :evening => '1',
+                  :night => '1'
+              }
+          ]
+      }
     end
-    shifts
+    prescription
   end
 end
 
